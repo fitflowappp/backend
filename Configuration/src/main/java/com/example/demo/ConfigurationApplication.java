63690c145f3fd30bb410ae6713f3580fd0747d84
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-08-22 17:47:57 +0800

    new configuration server

---
+++ Configuration/src/main/java/com/example/demo/ConfigurationApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationApplication.class, args);
+	}
+}
