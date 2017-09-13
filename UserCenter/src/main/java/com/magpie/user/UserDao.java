60b13f5d40513af73a86e9cd2d62e10c96117a67
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-13 09:30:00 +0800

    struct

---
+++ UserCenter/src/main/java/com/magpie/user/UserDao.java
@@ -0,0 +1,27 @@
+package com.magpie.user;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
+import org.springframework.stereotype.Repository;
+
+import com.magpie.base.dao.BaseMongoRepository;
+import com.magpie.user.model.User;
+
+@Repository
+public class UserDao extends BaseMongoRepository<User, Serializable> {
+
+	@Autowired
+	public UserDao(MongoRepositoryFactory mongoRepositoryFactory, MongoOperations mongoOps) {
+		super(mongoRepositoryFactory.getEntityInformation(User.class), mongoOps);
+	}
+
+	public User findOneByPhone(String phone) {
+		return findOneByQuery(new Query().addCriteria(Criteria.where("phone").is(phone)));
+	}
+	
+}
---
+++ UserCenter/src/main/java/com/magpie/user/model/User.java
@@ -0,0 +1,212 @@
+package com.magpie.user.model;
+
+import java.util.Date;
+
+import com.magpie.base.model.BaseModel;
+import com.magpie.share.ResourceRef;
+
+public class User extends BaseModel {
+	
+	private String id;
+	private String pcode;// 用户手机唯一码
+	private String name;
+	private String phone;
+	private String password;
+	private Date birthday;
+	private int age;
+	private int gender;// 1:男2:女
+
+
+	private ResourceRef headerImg;
+	private String headerImgUri;// 头像Uri
+
+	private ResourceRef fullImg;
+
+	private String signIntro;// 签名简介
+	private String intro;// 简介
+
+	private String role;// {#RoleType}
+	
+	private Date lastLoginDate;// 上次登录时间
+	private Date registerDate;// 注册时间
+	private Date latestMsgDate;// 最后发送消息的时间
+
+	private String from;// 来源
+
+	private boolean unRegistered;// 未注册
+	private boolean active;// 是否激活（用户首次打开app）
+
+	private String idfa;// 苹果手机的idfa或者安卓手机的IMEI号
+	private String idfaSource;// idfa来源{#IdfaSource}
+
+	private String client;// 手机系统
+
+	private String openId;// 
+	private String sessionKey;// 
+	
+	private boolean specialLogin;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPcode() {
+		return pcode;
+	}
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public ResourceRef getHeaderImg() {
+		return headerImg;
+	}
+	public void setHeaderImg(ResourceRef headerImg) {
+		this.headerImg = headerImg;
+	}
+	public String getHeaderImgUri() {
+		return headerImgUri;
+	}
+	public void setHeaderImgUri(String headerImgUri) {
+		this.headerImgUri = headerImgUri;
+	}
+	public ResourceRef getFullImg() {
+		return fullImg;
+	}
+	public void setFullImg(ResourceRef fullImg) {
+		this.fullImg = fullImg;
+	}
+	public String getSignIntro() {
+		return signIntro;
+	}
+	public void setSignIntro(String signIntro) {
+		this.signIntro = signIntro;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Date getLastLoginDate() {
+		return lastLoginDate;
+	}
+	public void setLastLoginDate(Date lastLoginDate) {
+		this.lastLoginDate = lastLoginDate;
+	}
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+	public Date getLatestMsgDate() {
+		return latestMsgDate;
+	}
+	public void setLatestMsgDate(Date latestMsgDate) {
+		this.latestMsgDate = latestMsgDate;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public boolean isUnRegistered() {
+		return unRegistered;
+	}
+	public void setUnRegistered(boolean unRegistered) {
+		this.unRegistered = unRegistered;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public String getIdfa() {
+		return idfa;
+	}
+	public void setIdfa(String idfa) {
+		this.idfa = idfa;
+	}
+	public String getIdfaSource() {
+		return idfaSource;
+	}
+	public void setIdfaSource(String idfaSource) {
+		this.idfaSource = idfaSource;
+	}
+	public String getClient() {
+		return client;
+	}
+	public void setClient(String client) {
+		this.client = client;
+	}
+	public String getOpenId() {
+		return openId;
+	}
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+	public String getSessionKey() {
+		return sessionKey;
+	}
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+	/**
+	 * @return the specialLogin
+	 */
+	public boolean isSpecialLogin() {
+		return specialLogin;
+	}
+	/**
+	 * @param specialLogin the specialLogin to set
+	 */
+	public void setSpecialLogin(boolean specialLogin) {
+		this.specialLogin = specialLogin;
+	}
+	
+}
