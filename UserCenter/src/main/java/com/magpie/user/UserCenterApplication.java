760a2c1ac89624a9f9cc0144a74d2dc5ee4f6b3c
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-14 15:08:42 +0800

    @EnableSwagger2Doc in application

--- UserCenter/src/main/java/com/magpie/user/UserCenterApplication.java
+++ UserCenter/src/main/java/com/magpie/user/UserCenterApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.didispace.swagger.EnableSwagger2Doc;
+
 @SpringBootApplication
-@ComponentScan(basePackages={"com.magpie"})  
+@EnableSwagger2Doc
+@ComponentScan(basePackages = { "com.magpie" })
 public class UserCenterApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(UserCenterApplication.class, args);
 	}
 }
--- Yoga/src/main/java/com/magpie/yoga/YogaApplication.java
+++ Yoga/src/main/java/com/magpie/yoga/YogaApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.didispace.swagger.EnableSwagger2Doc;
+
 @SpringBootApplication
-@ComponentScan(basePackages={"com.magpie"})  
+@EnableSwagger2Doc
+@ComponentScan(basePackages = { "com.magpie" })
 public class YogaApplication {
 
 	public static void main(String[] args) {
