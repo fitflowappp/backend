e8b8f24dcfedb75ef58fbd0c86d5e90178fe92fb
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-14 10:22:29 +0800

    new

---
+++ Yoga/src/main/java/com/magpie/yoga/YogaApplication.java
@@ -0,0 +1,14 @@
+package com.magpie.yoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.magpie"})  
+public class YogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaApplication.class, args);
+	}
+}
---
+++ Yoga/src/test/java/com/magpie/yoga/YogaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.magpie.yoga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YogaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
