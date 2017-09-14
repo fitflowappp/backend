af139925051df709c80f9381b48b977a972386c2
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-14 10:19:52 +0800

    user register

--- UserCenter/src/main/java/com/magpie/user/UserApi.java
+++ UserCenter/src/main/java/com/magpie/user/UserApi.java
@@ -2,6 +2,8 @@ package com.magpie.user;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,22 +19,21 @@ import io.swagger.annotations.ApiOperation;
 import com.magpie.base.view.BaseView;
 
 @RestController
-@RequestMapping(value="/user")
+@RequestMapping(value = "/user")
 public class UserApi {
 	@Autowired
 	private UserService userService;
-	
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public BaseView<?> register() {
-		
+
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	@ApiOperation(value = "用户注册")
+	public BaseView<?> register(@RequestBody SimpleRegUser simpleRegUser) {
 		User user = new User();
-		user.setPhone("18658193199");
-		user.setSpecialLogin(true);
+		BeanUtils.copyProperties(simpleRegUser, user);
+		user.setPassword(DigestUtils.md5Hex(simpleRegUser.getPassword()));
 		return userService.registerAnonymous(user);
-		
 	}
-	
+
 	/**
 	 * 功能：用户登录<br>
 	 * 
@@ -47,13 +48,13 @@ public class UserApi {
 	@ResponseBody
 	@ApiOperation(value = "用户登录")
 	public BaseView<?> login(@RequestBody SimpleRegUser simpleRegUser, HttpServletRequest request) {
-//		RequestParam requestParam = BaseUtil.getRequestParam(request);
-//		ClientCat client = versionInfoService.guessClientType(requestParam.getUserAgent());
-//		if (client == ClientCat.ANDROID || client == ClientCat.IPHONE) {
-//			simpleRegUser.setClient(client.getName());
-//		}
+		// RequestParam requestParam = BaseUtil.getRequestParam(request);
+		// ClientCat client =
+		// versionInfoService.guessClientType(requestParam.getUserAgent());
+		// if (client == ClientCat.ANDROID || client == ClientCat.IPHONE) {
+		// simpleRegUser.setClient(client.getName());
+		// }
 		return userService.login(simpleRegUser);
 	}
-	
-	
+
 }
