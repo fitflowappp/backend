f5ad308fa9d4fba971a67044184bcf9424e511a2
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-14 16:55:25 +0800

    add

--- UserCenter/src/main/java/com/magpie/user/UserApi.java
+++ UserCenter/src/main/java/com/magpie/user/UserApi.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.magpie.base.view.BaseView;
 import com.magpie.user.model.User;
 import com.magpie.user.req.SimpleRegUser;
+import com.magpie.user.view.UserView;
 
 import io.swagger.annotations.ApiOperation;
-
-import com.magpie.base.view.BaseView;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -27,7 +27,7 @@ public class UserApi {
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	@ApiOperation(value = "用户注册")
-	public BaseView<?> register(@RequestBody SimpleRegUser simpleRegUser) {
+	public BaseView<UserView> register(@RequestBody SimpleRegUser simpleRegUser) {
 		User user = new User();
 		BeanUtils.copyProperties(simpleRegUser, user);
 		user.setPassword(DigestUtils.md5Hex(simpleRegUser.getPassword()));
@@ -47,7 +47,7 @@ public class UserApi {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
 	@ApiOperation(value = "用户登录")
-	public BaseView<?> login(@RequestBody SimpleRegUser simpleRegUser, HttpServletRequest request) {
+	public BaseView<UserView> login(@RequestBody SimpleRegUser simpleRegUser, HttpServletRequest request) {
 		// RequestParam requestParam = BaseUtil.getRequestParam(request);
 		// ClientCat client =
 		// versionInfoService.guessClientType(requestParam.getUserAgent());
