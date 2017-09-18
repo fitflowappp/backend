23e86507532efd01760a1686d2f73bd931f0be58
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-18 10:59:54 +0800

    new act view

---
+++ Yoga/src/main/java/com/magpie/yoga/view/ActView.java
@@ -0,0 +1,24 @@
+package com.magpie.yoga.view;
+
+public class ActView {
+
+	private int type;
+	private String content;
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
