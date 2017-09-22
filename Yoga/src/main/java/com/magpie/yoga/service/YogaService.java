1e2db2ffeaf026d284e056d7a0d13514a2fdc64f
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-09-22 18:35:50 +0800

    avail

--- Yoga/src/main/java/com/magpie/yoga/service/YogaService.java
+++ Yoga/src/main/java/com/magpie/yoga/service/YogaService.java
@@ -172,6 +172,7 @@ public class YogaService {
 			} else {
 				avail = (last < HistoryEvent.SKIPPED.getCode() ? false : true);
 			}
+			w.setAvail(avail);
 			if (avail) {
 				lastWorkout = w;
 			}
