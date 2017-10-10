44bf3426a0baeb568cb78a0bd0f416e855a5b5de
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-10 10:16:25 +0800

    req

---
+++ Yoga/src/main/java/com/magpie/yoga/req/DashboardGenReq.java
@@ -0,0 +1,24 @@
+package com.magpie.yoga.req;
+
+import java.util.Date;
+
+public class DashboardGenReq {
+
+	private Date date;
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date
+	 *            the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
