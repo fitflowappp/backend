241c09e46a3ceed53b966e09973245a38999bae9
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-12 15:06:55 +0800

    facebook

---
+++ UserCenter/src/main/java/com/magpie/user/FacebookApi.java
@@ -0,0 +1,68 @@
+package com.magpie.user;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.magpie.base.view.BaseView;
+import com.magpie.base.view.Result;
+import com.magpie.session.ActiveUser;
+import com.magpie.share.UserRef;
+import com.magpie.user.req.FacebookReq;
+import com.magpie.user.view.UserView;
+
+import io.swagger.annotations.ApiOperation;
+
+@Controller
+@RequestMapping("/user")
+public class FacebookApi {
+
+	@Autowired
+	private UserService userService;
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	private Facebook facebook;
+	private ConnectionRepository connectionRepository;
+
+	public FacebookApi(Facebook facebook, ConnectionRepository connectionRepository) {
+		this.facebook = facebook;
+		this.connectionRepository = connectionRepository;
+	}
+
+	/**
+	 * 功能：用户登录<br>
+	 * 
+	 */
+	@RequestMapping(value = "/facebook/login", method = RequestMethod.POST)
+	@ResponseBody
+	@ApiOperation(value = "用户facebook登录")
+	public BaseView<UserView> login(@RequestBody FacebookReq facebookReq, @ActiveUser UserRef userRef,
+			HttpServletRequest request) {
+		org.springframework.social.facebook.api.User fbUser = facebook.userOperations().getUserProfile();
+		logger.info("get facebook user info: {}", JSON.toJSONString(fbUser));
+
+		if (fbUser == null || StringUtils.isEmpty(fbUser.getId())) {
+			return new BaseView<>(Result.FAILURE);
+		}
+		if (StringUtils.isEmpty(userRef.getId())) {
+			// 无session，重新注册/登录
+			return new BaseView<>(userService.loginIfRegistered(fbUser));
+		} else {
+			return new BaseView<>(userService.register(userRef.getId(), fbUser));
+		}
+
+	}
+
+}
--- UserCenter/src/main/java/com/magpie/user/UserApi.java
+++ UserCenter/src/main/java/com/magpie/user/UserApi.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.facebook.api.Facebook;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,14 +14,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSON;
 import com.magpie.base.view.BaseView;
 import com.magpie.base.view.Result;
 import com.magpie.session.ActiveUser;
 import com.magpie.share.UserRef;
 import com.magpie.user.constant.RoleType;
 import com.magpie.user.model.User;
-import com.magpie.user.req.FacebookReq;
 import com.magpie.user.req.SimpleRegUser;
 import com.magpie.user.view.UserView;
 
@@ -33,8 +30,6 @@ import io.swagger.annotations.ApiOperation;
 public class UserApi {
 	@Autowired
 	private UserService userService;
-	@Autowired
-	private Facebook facebook;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -65,30 +60,6 @@ public class UserApi {
 		// simpleRegUser.setClient(client.getName());
 		// }
 		return userService.login(simpleRegUser);
-	}
-
-	/**
-	 * 功能：用户登录<br>
-	 * 
-	 */
-	@RequestMapping(value = "/facebook/login", method = RequestMethod.POST)
-	@ResponseBody
-	@ApiOperation(value = "用户facebook登录")
-	public BaseView<UserView> login(@RequestBody FacebookReq facebookReq, @ActiveUser UserRef userRef,
-			HttpServletRequest request) {
-		org.springframework.social.facebook.api.User fbUser = facebook.userOperations().getUserProfile();
-		logger.info("get facebook user info: {}", JSON.toJSONString(fbUser));
-
-		if (fbUser == null || StringUtils.isEmpty(fbUser.getId())) {
-			return new BaseView<>(Result.FAILURE);
-		}
-		if (StringUtils.isEmpty(userRef.getId())) {
-			// 无session，重新注册/登录
-			return new BaseView<>(userService.loginIfRegistered(fbUser));
-		} else {
-			return new BaseView<>(userService.register(userRef.getId(), fbUser));
-		}
-
 	}
 
 	/**
