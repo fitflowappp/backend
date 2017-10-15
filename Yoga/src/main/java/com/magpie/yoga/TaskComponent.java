429bd7aec557c5edb0818059bad0179c0ed9fe5b
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-15 10:57:23 +0800

    @EnableScheduling

---
+++ Yoga/src/main/java/com/magpie/yoga/TaskComponent.java
@@ -0,0 +1,21 @@
+package com.magpie.yoga;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.magpie.yoga.service.YogaStatService;
+
+@Component
+@EnableScheduling
+public class TaskComponent {
+	@Autowired
+	private YogaStatService yogaStatService;
+
+	@Scheduled(cron = "0 30 0 * * *")
+	public void generateDashboard() {
+		yogaStatService.generatePrevDayDashboard();
+	}
+
+}
