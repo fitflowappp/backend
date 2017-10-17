87862e7f5d0058d28af907f5b88a95480ebdd283
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-17 11:36:20 +0800

    yoga getui

--- Yoga/src/main/java/com/magpie/getui/GetuiRegisterApi.java
+++ Yoga/src/main/java/com/magpie/getui/GetuiRegisterApi.java
@@ -18,7 +18,7 @@ import com.magpie.share.UserRef;
 import io.swagger.annotations.ApiOperation;
 
 @Controller
-@RequestMapping(value = "/getui")
+@RequestMapping(value = "/yoga/getui")
 public class GetuiRegisterApi {
 
 	@Autowired
