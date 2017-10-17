406974ee83c40c49972185a1014796b1670bc4b9
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-17 11:02:41 +0800

    new

---
+++ base/data/src/main/java/com/magpie/yoga/dao/UserStateDao.java
@@ -0,0 +1,43 @@
+package com.magpie.yoga.dao;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.support.MongoRepositoryFactory;
+import org.springframework.stereotype.Repository;
+
+import com.magpie.base.dao.BaseMongoRepository;
+import com.magpie.yoga.model.UserState;
+
+@Repository
+public class UserStateDao extends BaseMongoRepository<UserState, Serializable> {
+
+	@Autowired
+	public UserStateDao(MongoRepositoryFactory mongoRepositoryFactory, MongoOperations mongoOps) {
+		super(mongoRepositoryFactory.getEntityInformation(UserState.class), mongoOps);
+	}
+
+	public UserState findUserState(String uid) {
+		return findOneByQuery(new Query().addCriteria(Criteria.where("uid").is(uid)));
+	}
+
+	public void updateCurrentState(String uid, String currentChallengeId, String currentWorkoutId,
+			String currentRoutineId) {
+		Query query = new Query().addCriteria(Criteria.where("uid").is(uid));
+		updateFirst(query, new Update().set("currentChallengeId", currentChallengeId)
+				.set("currentWorkoutId", currentWorkoutId).set("currentRoutineId", currentRoutineId));
+	}
+
+	public void updateCurrentState(String uid, String currentChallengeId, String currentWorkoutId,
+			String currentRoutineId, int seconds) {
+		Query query = new Query().addCriteria(Criteria.where("uid").is(uid));
+		updateFirst(query,
+				new Update().set("currentChallengeId", currentChallengeId).set("currentWorkoutId", currentWorkoutId)
+						.set("currentRoutineId", currentRoutineId).set("currentRoutineSeconds", seconds));
+	}
+
+}
