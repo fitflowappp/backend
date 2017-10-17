ba1398a98dc70f545a0149837aa8c184fd2de90e
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-17 13:50:01 +0800

    role upgrade

--- UserCenter/src/main/java/com/magpie/user/AdminController.java
+++ UserCenter/src/main/java/com/magpie/user/AdminController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.magpie.base.query.PageQuery;
 import com.magpie.base.view.BaseView;
 import com.magpie.base.view.PageView;
+import com.magpie.user.constant.RoleType;
+import com.magpie.user.dao.UserDao;
 import com.magpie.user.req.AdminLoginReq;
 import com.magpie.user.view.AdminView;
 import com.magpie.user.view.FacebookUserView;
@@ -28,6 +30,8 @@ public class AdminController {
 	private AdminService adminService;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private UserDao userDao;
 
 	/**
 	 * 管理员登录
@@ -41,6 +45,32 @@ public class AdminController {
 	@ApiOperation(value = "用户登录", response = BaseView.class)
 	public BaseView<AdminView> login(@RequestBody AdminLoginReq admin, HttpServletRequest request) {
 		return adminService.login(admin.getName(), admin.getPassword());
+	}
+
+	/**
+	 * 升级为测试管理用户
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/user/{uid}/super", method = RequestMethod.PUT)
+	@ResponseBody
+	@ApiOperation(value = "升级为测试管理用户", response = BaseView.class)
+	public BaseView<?> upgradeUser(@PathVariable String uid) {
+		userDao.updateUserRole(uid, RoleType.ADMIN.getCode());
+		return new BaseView<>();
+	}
+
+	/**
+	 * 取消测试管理用户
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/user/{uid}/super", method = RequestMethod.DELETE)
+	@ResponseBody
+	@ApiOperation(value = "取消测试管理用户", response = BaseView.class)
+	public BaseView<?> cancelUpgradeUser(@PathVariable String uid) {
+		userDao.updateUserRole(uid, RoleType.CUSTOMER.getCode());
+		return new BaseView<>();
 	}
 
 	@RequestMapping(value = "/user", method = RequestMethod.GET)
