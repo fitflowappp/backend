2319035a51ef5616a6b8f3d1988fb511a995cd26
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-17 11:02:17 +0800

    de

--- UserCenter/src/main/java/com/magpie/user/view/FacebookUserView.java
+++ UserCenter/src/main/java/com/magpie/user/view/FacebookUserView.java
@@ -2,10 +2,13 @@ package com.magpie.user.view;
 
 import com.magpie.user.model.FaceBookUser;
 import com.magpie.user.model.User;
+import com.magpie.yoga.model.UserState;
 
 public class FacebookUserView extends FaceBookUser {
 
 	private User user;
+
+	private UserState userState;
 
 	public User getUser() {
 		return user;
@@ -13,6 +16,20 @@ public class FacebookUserView extends FaceBookUser {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	/**
+	 * @return the userState
+	 */
+	public UserState getUserState() {
+		return userState;
+	}
+
+	/**
+	 * @param userState the userState to set
+	 */
+	public void setUserState(UserState userState) {
+		this.userState = userState;
 	}
 
 }
