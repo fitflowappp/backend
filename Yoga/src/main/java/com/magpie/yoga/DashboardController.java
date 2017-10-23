44a29efad5f9af1d365e195795a63758551b1c1f
Jiangqinrao <Jiangqinrao@192.168.1.129>
2017-10-23 16:16:27 +0800

    das

--- Yoga/src/main/java/com/magpie/yoga/DashboardController.java
+++ Yoga/src/main/java/com/magpie/yoga/DashboardController.java
@@ -3,6 +3,8 @@ package com.magpie.yoga;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.magpie.base.utils.CsvUtils;
 import com.magpie.base.view.BaseView;
 import com.magpie.yoga.model.Dashboard;
 import com.magpie.yoga.req.DashboardGenReq;
@@ -29,6 +32,31 @@ public class DashboardController {
 	@ApiOperation(value = "get dashboard statistic data")
 	public List<Dashboard> getDashboard() {
 		return yogaStatService.getDashboard();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/csv")
+	@ResponseBody
+	@ApiOperation(value = "export csv")
+	public void exportCsv(HttpServletResponse response) {
+
+		StringBuffer sb = new StringBuffer();
+		sb.append("Facebook registration submitted").append(",").append("Facebook registration completed").append(",")
+				.append("Challenges started by unique users").append(",").append("Challenges completed by unique users")
+				.append(",").append("Workouts started by unique users").append(",")
+				.append("Workouts completed by unique users").append(",").append("Unique users who completed a Workout")
+				.append(",").append("Cumulative duration of videos watched").append(",")
+				.append("Unique users who turned scheduling on").append("Unique users who have received achievements")
+				.append(",").append("Unique users who have shared on social media").append("\r\n");
+
+		for (Dashboard board : yogaStatService.getDashboard()) {
+			sb.append(board.getFacebookRegSubmitNum()).append(",").append(board.getFacebookRegCompleteNum()).append(",")
+					.append(board.getChallengeStartNum()).append(",").append(board.getChallengeCompleteNum())
+					.append(",").append(board.getWorkoutStartNum()).append(",").append(board.getWorkoutCompleteNum())
+					.append(",").append(board.getOneWorkoutCompleteUserNum()).append(",")
+					.append(board.getTotalDuration()).append(",").append(board.getNotificationOnNum()).append(",")
+					.append(board.getAchievementNum()).append(",").append(board.getShareNum()).append("\r\n");
+		}
+		CsvUtils.download("dashboard.csv", sb.toString(), response);
 	}
 
 	@RequestMapping(value = "/day", method = RequestMethod.POST)
